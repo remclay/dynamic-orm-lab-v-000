8cacf3b4325ccec45c151f9d6be3c8eee25a0423
Remmy Clay <remmy.lauren@gmail.com>
2018-02-01 01:09:35 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ class InteractiveRecord
 
   def self.find_by(attribute_hash)
     value = attribute_hash.values.first
-    binding.pry
+  #  binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE '#{attribute_hash.keys.first}' = '#{value}'"
     DB[:conn].execute(sql)
   end
