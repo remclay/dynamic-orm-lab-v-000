e1b2265855042238d84b29cd868859b033d510c7
Remmy Clay <remmy.lauren@gmail.com>
2018-02-01 01:02:42 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,9 +54,8 @@ class InteractiveRecord
   end
 
   def self.find_by(attribute_hash)
-
-    value = attribute.values
-    attribute = attribute.key(values)
+    value = attribute_hash.values
+    attribute = attribute_hash.key(values)
     binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE '#{attribute}' = '#{attribute}'"
     DB[:conn].execute(sql)
